0cf0dbf86cd885cc2dd763d9320a6d32bc21920d
MiaMint <283266309+MiaMint-dev@users.noreply.github.com>
2026-08-28 16:08:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
 if (Days && Hours && Minutes && Seconds) {
-const targetDate = new Date("August 27 2026 21:30:00").getTime();
+const targetDate = new Date("September 03 2026 16:00:00").getTime();
 
 function timer () {
   const currentDate = new Date().getTime();

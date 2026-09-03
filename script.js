3c415cb6b213544db6b4542064af926ec311c47b
MiaMint <283266309+MiaMint-dev@users.noreply.github.com>
2026-09-02 20:45:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
 if (Days && Hours && Minutes && Seconds) {
-const targetDate = new Date("September 03 2026 18:00:00").getTime();
+const targetDate = new Date("September 03 2026 21:00:00").getTime();
 
 function timer () {
   const currentDate = new Date().getTime();
